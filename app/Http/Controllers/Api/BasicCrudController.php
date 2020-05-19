1ec62276560a063af2edc803e53c445535c0d5df
Paulo Mendonça <pauloandresoares@gmail.com>
2020-05-19 11:25:25 -0300

    Ajuste do teste d cadastro de vídeo

--- app/Http/Controllers/Api/BasicCrudController.php
+++ app/Http/Controllers/Api/BasicCrudController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use EloquentFilter\Filterable;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use Illuminate\Database\Query\Builder;
 
